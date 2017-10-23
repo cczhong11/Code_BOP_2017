aede23e429dc0a6288ca041c0b964444875f6f84
tianchen <tczhong24@gmail.com>
2017-10-22 21:58:31 -0400

    clear

--- fortest.py
+++
@@ -1,9 +0,0 @@
-import jieba.analyse
-import jieba.posseg as pseg
-str = "南投县立旭光高级中学在哪儿？"
-str1c = pseg.cut(str)
-ss = jieba.analyse.textrank(str)
-k = list(str1c)
-for i, j in k:
-    print(i+" "+j)
-print(ss)
